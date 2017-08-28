344df37438e264a94942495f8b17a5e255494b3d
niezl <nzlwan@126.com>
2017-08-28 13:42:04 +0800

    first commit

---
+++ GeoIPCity/Program.cs
@@ -0,0 +1,35 @@
+﻿using Subgurim.Controles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoIPCity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                string ip = Console.ReadLine();
+                Console.WriteLine(GetIPCity(ip).city);
+            } while (true);
+            Console.ReadKey();
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ipAddress"></param>
+        /// <returns></returns>
+        public static Location GetIPCity(string ipAddress)
+        {
+            string databasePath = @"E:\TeamFondationServers\GeoIPCity\GeoIPCity\APP_DATA\GeoLiteCity.dat";// HttpContext.Current.Server.MapPath("~/app_data/geocitylite.dat");
+            LookupService service = new LookupService(databasePath);
+            Location loc = service.getLocation(ipAddress); 
+            return loc;
+        }
+    }
+}
